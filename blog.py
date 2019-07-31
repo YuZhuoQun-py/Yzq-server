7868563e8f0c332c978b463895ecac9bb64644b5
YuZhuoQun-py <610832017@qq.com>
2019-07-31 20:35:28 +0800

    flask server

---
+++ blog.py
@@ -0,0 +1,37 @@
+class Blog:
+    def __init__(self, id, title, text):
+        self.id = id
+        self.title = title
+        self.text = text
+
+
+from blog import Blog
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+blogs = []
+b1 = Blog(1, 'hello1','zhangsan')
+b2 = Blog(2, 'hello2','lisi')
+blogs.append(b1)
+blogs.append(b2)
+
+@app.route('/')
+def home ():
+    return render_template('home.html')
+
+@app.route('/blogs')
+def lists():
+    return render_template('blogs.html',blogs = blogs)
+
+@app.route('/blog/<id>')
+def query_blog(id):
+    blog = None
+    for blg in blogs:
+        if blg.id == int(id):
+            blog = blg
+    return render_template('blog.html',blog = blog)
+
+
+# if __name__ == '__main__':
+#     app.run(debug = True)
